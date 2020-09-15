f64035df0de473ab0406bfba1b74c8a0574d30a3
김진실 <kjin_048@naver.com>
2020-09-15 17:30:33 +0900

    로그인 form 구현 [수정]

--- frontend/src/views/user/Login.vue
+++ frontend/src/views/user/Login.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="my-auto"> 
+    <div class="mt-3 mr-5 text-end">
+      <v-btn rounded color="error" dark>회원가입</v-btn>
+
+  <div style="margin-top : 100px;"> 
     <v-container >
           <h2 class="text-center mb-3">로그인</h2>
           <v-form>
@@ -16,24 +19,25 @@
                         filled
                         rounded              
                       ></v-text-field>
+                      <v-btn rounded color="primary" style="width:370px; height:50px;">로그인</v-btn>
+
                       <div class="d-flex justify-end">
                         <v-btn text>비회원으로 이용하기</v-btn>
                       </div>
-                      <div class="mt-4 text-center">
-                        <v-btn rounded color="primary" dark class="mr-3">로그인</v-btn>
-                        <v-btn rounded color="error" dark>회원가입</v-btn>
-                      </div>
+                      <hr class="mt-5">
+                      <h5 style="color: gray;" class="text-left">카카오로 로그인하기</h5>
                 </v-col>
             </v-row>
           </v-form>
       </v-container>
   </div>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  
+  }
 </script>
 
 <style>
