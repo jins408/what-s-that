81c5fa0ce85e78d5be56673c445db73278115c03
김진실 <kjin_048@naver.com>
2020-09-15 16:57:52 +0900

    로그인 form 구현

--- frontend/src/views/user/Login.vue
+++ frontend/src/views/user/Login.vue
@@ -1,5 +1,33 @@
 <template>
-  <p>login</p>
+  <div class="my-auto"> 
+    <v-container >
+          <h2 class="text-center mb-3">로그인</h2>
+          <v-form>
+            <v-row >
+              <v-col class="mx-auto"  cols="12" sm="4">
+                      <v-text-field
+                        placeholder="아이디(이메일)입력해주세요."
+                        filled
+                        rounded                    
+                      ></v-text-field>         
+                      <v-text-field
+                        type="password"
+                        placeholder="영문, 숫자 혼용 8자 이상"
+                        filled
+                        rounded              
+                      ></v-text-field>
+                      <div class="d-flex justify-end">
+                        <v-btn text>비회원으로 이용하기</v-btn>
+                      </div>
+                      <div class="mt-4 text-center">
+                        <v-btn rounded color="primary" dark class="mr-3">로그인</v-btn>
+                        <v-btn rounded color="error" dark>회원가입</v-btn>
+                      </div>
+                </v-col>
+            </v-row>
+          </v-form>
+      </v-container>
+  </div>
 </template>
 
 <script>
