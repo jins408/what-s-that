4a2e15063a884a805f1e9bb6a95f97af4c7eea8e
김석환 <tokany@naver.com>
2020-09-15 15:30:16 +0900

    create login join

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,9 +1,23 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+// Join
+import Join from '../views/user/Join.vue'
+import Login from '../views/user/Login.vue'
+
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '/user/join',
+    name: Join,
+    component: Join
+  },
+  {
+    path: '/user/login',
+    name: Login,
+    component: Login
+  },
   
 ]
 
---
+++ frontend/src/views/user/Join.vue
@@ -0,0 +1,13 @@
+<template>
+  <p>join</p>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
