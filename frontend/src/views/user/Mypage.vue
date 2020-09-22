86bdc4f8537c5e4271ca3659b40ce7872175c44c
김진실 <kjin_048@naver.com>
2020-09-22 13:12:06 +0900

    마이페이지 수정

--- frontend/src/views/user/Mypage.vue
+++ frontend/src/views/user/Mypage.vue
@@ -1,9 +1,10 @@
 <template>
+<div style="margin: 10rem 0;">
   <v-card
     class="mx-auto"
     outlined
     width="90%"
-    style="margin-top: 10rem;"
+    style="margin-bottom: 2rem;"
   >
 
   <div class="d-flex justify-content-between">
@@ -39,12 +40,55 @@
                 </v-row>
             </v-list-item-content>            
         </v-list-item>
-    
   </v-card >
+    
+    <v-card
+        class="mx-auto" 
+        width="90%">
+    <v-tabs
+      background-color="white"
+      color="deep-purple accent-4"
+      left
+    >
+      <v-tab>찜목록</v-tab>
+      <v-tab>카테고리</v-tab>
+      
+      <v-tab>내정보</v-tab>
+
+      <v-tab-item
+        v-for="n in 3"
+        :key="n"
+      >
+      <category v-if="n==2" />
+        <v-container fluid>
+          <v-row>
+            <v-col
+              v-for="i in 6"
+              :key="i"
+              cols="12"
+              md="4"
+            >
+              <!-- <v-img
+                :src="`https://picsum.photos/500/300?image=${i * n * 5 + 10}`"
+                :lazy-src="`https://picsum.photos/10/6?image=${i * n * 5 + 10}`"
+                aspect-ratio="1"
+              ></v-img> -->
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-tab-item>
+    </v-tabs>
+  </v-card>
+  </div>
 </template>
 
 <script>
+import category from "../../components/Category.vue"
+
 export default {
+  components:{
+    category,
+  }
 
 }
 </script>
