43092912bc1f95549e1b4207fa7a63a8827e4d69
김석환 <tokany@naver.com>
2020-09-22 14:53:51 +0900

    add vuex

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,12 +1,18 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
+import store from './store'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify';
+
+Vue.use(vuetify)
+Vue.use(Vuex)
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   vuetify,
+  store,
   render: h => h(App)
 }).$mount('#app')
