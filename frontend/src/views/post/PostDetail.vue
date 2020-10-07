dfdfbb709f5ff11b1eb6db3e631451e4d982d69a
윤영현 <yo950225@gmail.com>
2020-10-07 18:51:13 +0900

    201007 Frontend update

--- frontend/src/views/post/PostDetail.vue
+++ frontend/src/views/post/PostDetail.vue
@@ -27,7 +27,7 @@
             @click="bookmarkdelete"
           ></i>
         </div>
-        <div id="roadview" style="width:500px;height:400px;"></div>
+        <div id="map" style="width:500px;height:400px;"></div>
         <div> 
           <Viewer v-if="post.content != null" :initialValue="post.content" />           
             <!-- {{post.content}} -->
@@ -99,17 +99,19 @@ export default {
 
   methods: {
     initMap() {
-      var roadviewContainer = document.getElementById('roadview'); //로드뷰를 표시할 div
-      var roadview = new kakao.maps.Roadview(roadviewContainer); //로드뷰 객체
-      var roadviewClient = new kakao.maps.RoadviewClient(); //좌표로부터 로드뷰 파노ID를 가져올 로드뷰 helper객체
-      var geocoder = new kakao.maps.services.Geocoder();
-      
-      var position = new kakao.maps.LatLng(33.450701, 126.570667);
-
-      // 특정 위치의좌표와 가까운 로드뷰의 panoId를 추출하여 로드뷰를 띄운다.
-      roadviewClient.getNearestPanoId(position, 50, function(panoId) {
-          roadview.setPanoId(panoId, position); //panoId와 중심좌표를 통해 로드뷰 실행
-      });
+      let lng = this.post.lng;
+      let lat = this.post.lat;
+      var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+      mapOption = { 
+          center: new kakao.maps.LatLng(lng, lat), // 지도의 중심좌표
+          level: 3 // 지도의 확대 레벨
+      };
+      var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+      var markerPosition  = new kakao.maps.LatLng(lng, lat);
+      var marker = new kakao.maps.Marker({
+         position: markerPosition
+      }); 
+      marker.setMap(map);
     },
     addScript() {
       const script = document.createElement('script');
