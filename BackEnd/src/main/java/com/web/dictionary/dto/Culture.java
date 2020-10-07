31982e8efcd44fecbeb3f9bf7d066ef416238435
ldu8183 <ldu8183@naver.com>
2020-10-07 17:38:12 +0900

    culture detail update

--- BackEnd/src/main/java/com/web/dictionary/dto/Culture.java
+++ BackEnd/src/main/java/com/web/dictionary/dto/Culture.java
@@ -12,4 +12,10 @@ public class Culture {
 	private int isfavorite;
 	private String imageUrl;
 	private String ename;
+	private String categoty;
+	private String generation;
+	private String constructionperiod;
+	private String location;
+	private double lng;
+	private double lat;
 }
